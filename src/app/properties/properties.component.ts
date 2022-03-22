44be5ebbe87daed40f6dfa79d45ddb4a628be65c
rochbu <rob@buskens.ca>
2022-03-22 09:05:32 -0500

    3 files modified

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-properties',
-  template: './properties.component.html',
+  templateUrl: './properties.component.html',
   styleUrls: ['./properties.component.css'],
 })
 export class PropertiesComponent implements OnInit {
