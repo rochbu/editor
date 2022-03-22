72f4a278b00b4f5a6791ef107559007b3569712f
rochbu <rob@buskens.ca>
2022-03-22 08:32:13 -0500

    2 files modified

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -3,11 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-properties',
   template: `
-  <select class="form-select" aria-label="Default select example">
-    <option value="square">Square</option>
-    <option value="circle" selected>Circle</option>
-    <option value="rectangle">Rectangle</option>
-  </select>`,
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item" role="presentation">
+      <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Item</button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Text</button>
+    </li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+      <div class="tab-pane active" id="item" role="tabpanel" aria-labelledby="home-tab">
+        <select class="form-select" aria-label="Default select example">
+        <option value="square">Square</option>
+        <option value="circle" selected>Circle</option>
+        <option value="rectangle">Rectangle</option>
+      </select>
+    </div>
+    <div class="tab-pane" id="text" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  </div>
+  `,
   styleUrls: ['./properties.component.css'],
 })
 export class PropertiesComponent implements OnInit {
